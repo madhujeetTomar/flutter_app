b4850cafb77d067e882e76ae3198f2997087fe6a
Madhujeet Singh Tomer <emb-madhtom@emb-ban-398.embitel.local>
2019-11-01 01:09:48 +0530

    Demo for image

--- lib/main.dart
+++ lib/main.dart
@@ -14,25 +14,20 @@ void main() {
 */
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("hello"),
-          backgroundColor: Colors.blueGrey[900],
-          actions: <Widget>[BackButton()],
-        ),
-        body: Center(
-          child: Text(
-            "My name is Madhujeet",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontWeight: FontWeight.bold),
-            overflow: TextOverflow.ellipsis,
-
-          ),
-        ),
-        backgroundColor: Colors.teal,
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("hello"),
+        backgroundColor: Colors.blueGrey[900],
+        actions: <Widget>[BackButton()],
       ),
+      body: Center(
+        child: Image(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1538611023136-84b24b785b1d?ixlib='
+                'rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60')),
+      ),
+      backgroundColor: Colors.teal,
     ),
-  );
+  ));
 }
